12216e906111529161c168a7b10bc40c75fe6dad
jeanpitad@yahoo.com <jeanpitad@yahoo.com>
2023-02-23 17:35:54 -0500

    draft models

--- virotour/flask/app/models.py
+++ virotour/flask/app/models.py
@@ -68,9 +68,7 @@ class State(db.Model):
     
     state_id = db.Column(db.String, primary_key=True)
     setting = db.Column(db.String(255), nullable=False)
-    filter = db.Column(db.String(255)) 
-    
-    images  = db.relationship('Images', backref='State', lazy=True)
+    filter_id = db.Column(db.String(255), db.ForeignKey('filter_id'), nullable=False) 
     
     def __init__(self, state_id, setting, filter):
         self.state_id = state_id
@@ -79,4 +77,73 @@ class State(db.Model):
 
     def __repr__(self):
         return '<State %r>' % self.setting
+    
+class Filters(db.Model):
+    __tablename__ = 'filter_table_v1'
+    
+    filter_id = db.Column(db.String, primary_key=True)
+    type = db.Column(db.String(255), nullable=False)
+    settings = db.Column(db.String(max), nullable=False) 
+    
+    def __init__(self, type, settings):
+        self.type = type
+        self.settings = settings
+
+    def __repr__(self):
+        return '<Filters %r>' % self.settings
+    
+class Informational_Hotshots(db.Model):
+    __tablename__ = 'Informational_Hotshots_table_v1'
+    
+    Informational_id = db.Column(db.String, primary_key=True)
+    position_x = db.Column(db.Integer, nullable=False)
+    position_y = db.Column(db.Integer, nullable=False)
+    position_z = db.Column(db.Integer, nullable=False)
+    content = db.Column(db.String(max)) 
+    
+    def __init__(self, position_x, position_y, position_z, content):
+        self.position_x = position_x
+        self.position_y = position_y
+        self.position_z = position_z
+        self.content = content
+
+    def __repr__(self):
+        return '<Informational_Hotshots %r>' % self.content
+    
+class Transitional_Hotshots(db.Model):
+    __tablename__ = 'Transitional_Hotshots_table_v1'
+    
+    Transitional_id = db.Column(db.String, primary_key=True)
+    position_x = db.Column(db.Integer, nullable=False)
+    position_y = db.Column(db.Integer, nullable=False)
+    position_z = db.Column(db.Integer, nullable=False)
+    content = db.Column(db.String(max)) 
+    
+    def __init__(self, position_x, position_y, position_z, content):
+        self.position_x = position_x
+        self.position_y = position_y
+        self.position_z = position_z
+        self.content = content
+
+    def __repr__(self):
+        return '<Transitional_Hotshots %r>' % self.content
+    
+class Text(db.Model):
+    __tablename__ = 'Text_table_v1'
+    
+    Text_id = db.Column(db.String, primary_key=True)
+    position_x = db.Column(db.Integer, nullable=False)
+    position_y = db.Column(db.Integer, nullable=False)
+    position_z = db.Column(db.Integer, nullable=False)
+    content = db.Column(db.String(max)) 
+    
+    def __init__(self, position_x, position_y, position_z, content):
+        self.position_x = position_x
+        self.position_y = position_y
+        self.position_z = position_z
+        self.content = content
+
+    def __repr__(self):
+        return '<Text %r>' % self.content
+    
     
