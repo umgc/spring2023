5550aa0620bee6e24463b9f15266ae638dee1ab5
Ivelin Tchangalov <i.tchangalov@gmail.com>
2023-02-25 15:54:24 -0500

    Add Validation for Team A Request: Access-Control-Allow-Origin

--- virotour/flask/app/tests/tour/test_hello_world.py
+++ virotour/flask/app/tests/tour/test_hello_world.py
@@ -6,4 +6,5 @@ def test_hello_world(client):
     resp = client.get('/api/')
     data = parse_http_response(resp)
     assert resp.status_code == 200
+    assert resp.headers['Access-Control-Allow-Origin'] == '*'
     assert 'Hello World! This is the REST APIs starter template.' in data['message']
