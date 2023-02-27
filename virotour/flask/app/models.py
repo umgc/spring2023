35d6062e5cfca206b8536cab2f269f7854d5abe6
khuynh16 <kelvinahuynh@gmail.com>
2023-02-26 22:50:53 -0500

    added extracted text model

--- virotour/flask/app/models.py
+++ virotour/flask/app/models.py
@@ -37,8 +37,26 @@ class Image(db.Model):
     def __init__(self, location_id, file_path):
         self.location_id = location_id
         self.file_path = file_path
-
-
+    
     def __repr__(self):
         return '<Images %r>' % self.image_id
-    
+
+class Text(db.Model):
+    __tablename__ = 'extracted_text_table_v1'
+
+    text_id = db.Column(db.Integer, primary_key=True)
+    location_id = db.Column(db.Integer)
+    text_content = db.Column(db.String(255))
+    text_xcoord = db.Column(db.Float)
+    text_ycoord = db.Column(db.Float)
+    text_zcoord = db.Column(db.Float)
+
+    def __init__(self, location_id, text_content, text_xcoord, text_ycoord, text_zcoord):
+        self.location_id = location_id
+        self.text_content = text_content
+        self.text_xcoord = text_xcoord
+        self.text_ycoord = text_ycoord
+        self.text_zcoord = text_zcoord
+
+    def __repr__(self):
+        return '<Text %r>' % self.text_id
