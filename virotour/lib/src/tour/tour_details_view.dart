39230cef20f8736b9e602c6bf92cdda445d1da97
Nick <49327729+tpbnick@users.noreply.github.com>
2023-03-11 13:48:36 -0500

    Fixed movement issue

--- virotour/lib/src/tour/tour_details_view.dart
+++ virotour/lib/src/tour/tour_details_view.dart
@@ -20,71 +20,76 @@ class _TourDetailsViewState extends State<TourDetailsView> {
     _panoramaUrl2 = 'https://i.imgur.com/yrhSmSh.jpg';
   }
 
+  void _onTapHotspot() {
+    if (_panoramaUrl == _panoramaUrl2) {
+      setState(() {
+        _panoramaUrl = 'https://i.imgur.com/O9CBhdM.jpg';
+      });
+    } else {
+      setState(() {
+        _panoramaUrl = _panoramaUrl2;
+      });
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Tour Details / Tour Name'),
       ),
-      body: GestureDetector(
-        onTap: () {
-          setState(() {
-            if (_panoramaUrl == _panoramaUrl2) {
-              _panoramaUrl = 'https://i.imgur.com/O9CBhdM.jpg';
-            } else {
-              _panoramaUrl = _panoramaUrl2;
-            }
-          });
-        },
-        child: Panorama(
-          sensitivity: 2.0,
-          child: Image.network(_panoramaUrl),
-          hotspots: _panoramaUrl == _panoramaUrl2
-              ? [
-                  Hotspot(
-                    latitude: -190,
-                    longitude: -60,
-                    width: 60.0,
-                    height: 60.0,
-                    widget: Container(
-                      decoration: BoxDecoration(
-                        color: const Color.fromARGB(255, 235, 80, 80),
-                        borderRadius: BorderRadius.circular(30.0),
-                      ),
-                      child: IconButton(
-                        icon: Icon(Icons.arrow_downward),
-                        onPressed: () {
-                          setState(() {
-                            _panoramaUrl = 'https://i.imgur.com/O9CBhdM.jpg';
-                          });
-                        },
+      body: Stack(
+        children: [
+          Panorama(
+            sensitivity: 2.0,
+            hotspots: _panoramaUrl == _panoramaUrl2
+                ? [
+                    Hotspot(
+                      latitude: -190,
+                      longitude: -60,
+                      width: 60.0,
+                      height: 60.0,
+                      widget: Container(
+                        decoration: BoxDecoration(
+                          color: const Color.fromARGB(255, 235, 80, 80),
+                          borderRadius: BorderRadius.circular(30.0),
+                        ),
+                        child: IconButton(
+                          icon: const Icon(Icons.arrow_downward),
+                          onPressed: _onTapHotspot,
+                        ),
                       ),
                     ),
-                  ),
-                ]
-              : [
-                  Hotspot(
-                    latitude: -170.0,
-                    longitude: 90.0,
-                    width: 60.0,
-                    height: 60.0,
-                    widget: Container(
-                      decoration: BoxDecoration(
-                        color: const Color.fromARGB(255, 235, 80, 80),
-                        borderRadius: BorderRadius.circular(30.0),
-                      ),
-                      child: IconButton(
-                        icon: Icon(Icons.arrow_downward),
-                        onPressed: () {
-                          setState(() {
-                            _panoramaUrl = _panoramaUrl2;
-                          });
-                        },
+                  ]
+                : [
+                    Hotspot(
+                      latitude: -170.0,
+                      longitude: 90.0,
+                      width: 60.0,
+                      height: 60.0,
+                      widget: Container(
+                        decoration: BoxDecoration(
+                          color: const Color.fromARGB(255, 235, 80, 80),
+                          borderRadius: BorderRadius.circular(30.0),
+                        ),
+                        child: IconButton(
+                          icon: const Icon(Icons.arrow_downward),
+                          onPressed: _onTapHotspot,
+                        ),
                       ),
                     ),
-                  ),
-                ],
-        ),
+                  ],
+            child: Image.network(_panoramaUrl),
+          ),
+          IgnorePointer(
+            child: GestureDetector(
+              onTap: () {},
+              child: Container(
+                color: Colors.transparent,
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
