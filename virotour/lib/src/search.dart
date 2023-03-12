d40869bb784d3118a549414f4f754b2726f74f64
Nick <49327729+tpbnick@users.noreply.github.com>
2023-03-11 21:59:37 -0500

    Updated to use api/tours endpoint for data

--- virotour/lib/src/search.dart
+++ virotour/lib/src/search.dart
@@ -1,37 +1,39 @@
+import 'dart:convert';
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 import 'package:virotour/src/tour/tour_details_view.dart';
 
 class SearchPage extends StatefulWidget {
-  const SearchPage({super.key});
+  const SearchPage({Key? key});
 
   @override
   _SearchPageState createState() => _SearchPageState();
 }
 
 class _SearchPageState extends State<SearchPage> {
-  late String searchQuery;
-  // we will need to create a function here that grabs ALL the tour data for us to filter through
-  List<dynamic> mockItems = [
-    {
-      "name": "Virtual Tour Test 1",
-      "description": "This is the first tour",
-      "id": 1
-    },
-    {
-      "name": "Virtual Tour Test 2",
-      "description": "This happens to be the second tour",
-      "id": 2
-    },
-    {
-      "name": "Virtual Tour Test 3",
-      "description": "Look out, here comes the third tour",
-      "id": 3
-    }
-  ];
-  List<dynamic> filteredItems = [];
+  String searchQuery = "";
   List<Map<String, dynamic>> items = [];
+  List<dynamic> filteredItems = [];
 
   final _searchController = TextEditingController();
+
+  Future<void> fetchData() async {
+    final response =
+        await http.get(Uri.parse('http://127.0.0.1:8081/api/tours'));
+
+    if (response.statusCode == 200) {
+      final data = jsonDecode(response.body) as Map<String, dynamic>;
+      final dataLength = data["tours"].length;
+
+      final tours = data['tours'] as List<dynamic>;
+
+      setState(() {
+        items = tours.map((tour) => tour as Map<String, dynamic>).toList();
+      });
+    } else {
+      throw Exception("Failed to load tour data!");
+    }
+  }
 
   void searchData() {
     setState(() {
@@ -59,8 +61,7 @@ class _SearchPageState extends State<SearchPage> {
   @override
   void initState() {
     super.initState();
-    items = mockItems.map((e) => e as Map<String, dynamic>).toList();
-    filteredItems = [];
+    fetchData();
   }
 
   @override
@@ -96,23 +97,15 @@ class _SearchPageState extends State<SearchPage> {
               ],
             ),
             const SizedBox(height: 16.0),
-            Expanded(
-              child: ListView.builder(
-                itemCount: filteredItems.length,
-                itemBuilder: (BuildContext context, int index) {
-                  return Padding(
-                    padding: const EdgeInsets.symmetric(vertical: 8.0),
-                    child: GestureDetector(
-                      onTap: () {
-                        Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                            builder: (context) => const TourDetailsView(),
-                          ),
-                        );
-                      },
-                      child: ElevatedButton(
-                        onPressed: () {
+            if (searchQuery.isNotEmpty)
+              Expanded(
+                child: ListView.builder(
+                  itemCount: filteredItems.length,
+                  itemBuilder: (BuildContext context, int index) {
+                    return Padding(
+                      padding: const EdgeInsets.symmetric(vertical: 8.0),
+                      child: GestureDetector(
+                        onTap: () {
                           Navigator.push(
                             context,
                             MaterialPageRoute(
@@ -120,18 +113,27 @@ class _SearchPageState extends State<SearchPage> {
                             ),
                           );
                         },
-                        child: ListTile(
-                          title: Text(filteredItems[index]['name'] as String),
-                          subtitle: Text(
-                            filteredItems[index]['description'] as String,
+                        child: ElevatedButton(
+                          onPressed: () {
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                builder: (context) => const TourDetailsView(),
+                              ),
+                            );
+                          },
+                          child: ListTile(
+                            title: Text(filteredItems[index]['name'] as String),
+                            subtitle: Text(
+                              filteredItems[index]['description'] as String,
+                            ),
                           ),
                         ),
                       ),
-                    ),
-                  );
-                },
+                    );
+                  },
+                ),
               ),
-            ),
           ],
         ),
       ),
