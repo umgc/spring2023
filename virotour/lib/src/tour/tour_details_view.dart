b1b57bf1a9cac2221e678e81573f0666ba884885
Nick <49327729+tpbnick@users.noreply.github.com>
2023-03-12 19:13:04 -0400

    Attempting to make Pannellum work with HTML

--- virotour/lib/src/tour/tour_details_view.dart
+++ virotour/lib/src/tour/tour_details_view.dart
@@ -8,7 +8,6 @@ import 'package:http/http.dart' as http;
 import '../helpers/ip_handler.dart';
 import '../settings/settings_view.dart';
 
-/// Displays detailed information about a Tour.
 class TourDetailsView extends StatefulWidget {
   const TourDetailsView({super.key, required this.tour});
   static const routeName = '/tour';
@@ -35,6 +34,8 @@ class _TourDetailsViewState extends State<TourDetailsView> {
     }
   }
 
+  static const tourHtml = "<h2> Hello, world! </h2>";
+
   @override
   Widget build(BuildContext context) {
     final width = MediaQuery.of(context).size.width;
@@ -44,56 +45,25 @@ class _TourDetailsViewState extends State<TourDetailsView> {
     final safeHeight = height - padding.top - padding.bottom;
     final safeWidth = width - padding.left - padding.right;
 
-    // TODO: Mock returned object from GET /tour/<tour_id>
-    const Map<String, dynamic> mockTourInfo = <String, dynamic>{
-      "tour_id": "tour_1",
-      "start_location": "location_1",
-      "locations": {
-        "location_1": {
-          "url":
-              "https://cdn.pannellum.org/2.5/pannellum.htm#panorama=https%3A//i.imgur.com/O9CBhdM.jpg&autoLoad=true",
-          "adjacent_locations": {"location_2", "location_3"}
-        },
-        "location_2": {
-          "url":
-              "https://cdn.pannellum.org/2.5/pannellum.htm#panorama=https%3A//i.imgur.com/O9CBhdM.jpg&autoLoad=true",
-          "adjacent_locations": {"location_1"}
-        },
-        "location_3": {
-          "url":
-              "https://cdn.pannellum.org/2.5/pannellum.htm#panorama=https%3A//i.imgur.com/O9CBhdM.jpg&autoLoad=true",
-          "adjacent_locations": {"location_1"}
-        }
-      }
-    };
-    final String startURL = mockTourInfo['locations']
-            [mockTourInfo['start_location']]['url']
-        .toString();
+    final arguments =
+        ModalRoute.of(context)!.settings.arguments as Map<String, dynamic>;
+    final tourId = arguments['id'] as String;
+    final tourName = arguments['name'] as String;
+
     return Scaffold(
       appBar: AppBar(
-        // TODO: The tour name should come from the tour object
-        title: Text(widget.tour.tourName),
-        actions: [
-          IconButton(
-            icon: const Icon(Icons.settings),
-            onPressed: () {
-              Navigator.restorablePushNamed(context, SettingsView.routeName);
-            },
-          ),
-        ],
+        title: Text(tourName),
       ),
       body: WebViewX(
         height: safeHeight,
         width: safeWidth,
-        // TODO: replace URL with response from API call GET /tour/<tour_id>/
-        initialContent: startURL,
+        initialContent: tourHtml,
         onPageStarted: (url) {
-          // This method is called when the WebView starts loading a new page
           debugPrint('Page started loading: $url');
         },
-        onWebViewCreated: (controller) {
-          webviewController = controller;
-        },
+        onWebResourceError: (error) {
+          debugPrint('WebViewX error: ${error.description}');
+        }, // Set the background color
       ),
     );
   }
