5af9cfa72bdf550e1e05331d873698263856a5d7
Ivelin Tchangalov <i.tchangalov@gmail.com>
2023-03-21 17:45:58 -0400

    Update app.db

--- virotour_local/flask/virotour/integration_tests/test_basic_integration.py
+++ virotour_local/flask/virotour/integration_tests/test_basic_integration.py
@@ -7,7 +7,7 @@ from virotour.tests.common_utils import get_image_paths, get_image_path
 
 # api-endpoint
 URL = "https://virotour2023-flask-server.azurewebsites.net/api"
-#URL = "http://127.0.0.1:8081/api"
+# URL = "http://127.0.0.1:8081/api"
 
 
 def file_upload_request_builder(image_paths):
