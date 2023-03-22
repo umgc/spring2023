2aa410e1c231f1f9a32015d47c0c266549a4ef74
Viet Nguyen <vietjul@gmail.com>
2023-03-21 22:50:43 -0400

    Team a dev joint dev (#88)
    * feat: Handle Azure server

--- virotour/lib/src/helpers/ip_handler.dart
+++ virotour/lib/src/helpers/ip_handler.dart
@@ -1,15 +1,26 @@
 import 'dart:convert';
 
+import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'package:http/http.dart';
 
 class IPHandler {
-  static const List<String> _ipsToTry = ['192.168.1.180', '127.0.0.1'];
+  static const List<String> _ipsToTry = [
+    'virotour2023-flask-server.azurewebsites.net',
+    '192.168.1.180',
+    '127.0.0.1'
+  ];
   static const int _port = 8081;
 
   Future<http.Response> get(String endpoint) async {
     for (final String ip in _ipsToTry) {
-      final String url = 'http://$ip:$_port$endpoint';
+      String url;
+
+      if (ip.contains('azurewebsites.net')) {
+        url = 'https://$ip$endpoint';
+      } else {
+        url = 'http://$ip:$_port$endpoint';
+      }
 
       try {
         final http.Response response = await http.get(Uri.parse(url));
@@ -19,7 +30,8 @@ class IPHandler {
           return response;
         }
       } catch (e) {
-        print('Error connecting to $url: $e');
+        debugPrint('Error connecting to $url: $e');
+        throw 'Error connecting to $url: $e';
       }
     }
 
--- virotour_local/flask/app/tests/test_sitemap.py
+++
@@ -1,8 +0,0 @@
-from app.tests.common_utils import parse_http_response
-
-
-def test_get_sitemap(client):
-    for item in sorted(parse_http_response(client.get(f'/api/site-map')).items()):
-        request_type = "/".join(item[1])
-        request_path = item[0]
-        print(f"{request_type} {request_path}")
