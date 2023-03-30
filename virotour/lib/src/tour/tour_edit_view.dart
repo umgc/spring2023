ce734d8f79665e63589e1ea2640476abdf5195d9
Viet <vietjul@gmail.com>
2023-03-30 11:29:48 -0400

    refactor: Ignore API CRUDs that are not ready

--- virotour/lib/src/tour/tour_edit_view.dart
+++ virotour/lib/src/tour/tour_edit_view.dart
@@ -1,10 +1,7 @@
-import 'dart:convert';
-
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
+import 'package:virotour/src/helpers/ip_handler.dart';
 import 'package:virotour/src/tour/tour.dart';
-
-import '../helpers/ip_handler.dart';
 
 class TourEditView extends StatefulWidget {
   static const routeName = '/tour_edit';
@@ -89,40 +86,41 @@ class _TourEditViewState extends State<TourEditView> {
                   const SizedBox(width: 16.0),
                   ElevatedButton(
                     onPressed: () async {
-                      final String body = {
-                        'id': widget.tour.id,
-                        'name': _nameController.text,
-                        'description': _descriptionController.text,
-                      }.toString();
+                      // Will update if and when the API endpoint is ready
+                      // final String body = {
+                      //   'id': widget.tour.id,
+                      //   'name': _nameController.text,
+                      //   'description': _descriptionController.text,
+                      // }.toString();
 
-                      final String headers =
-                          {'Content-Type': 'application/json'}.toString();
+                      // final String headers =
+                      //     {'Content-Type': 'application/json'}.toString();
 
-                      final Map<String, String> options = {
-                        'headers': headers,
-                        'body': body,
-                      };
+                      // final Map<String, String> options = {
+                      //   'headers': headers,
+                      //   'body': body,
+                      // };
 
-                      final http.Response response = await IPHandler()
-                          .post('/api/update/tour/${widget.tour.id}', options);
-                      if (response.statusCode == 200) {
-                        Navigator.pop(context);
-                      } else {
-                        showDialog(
-                          context: context,
-                          builder: (context) => AlertDialog(
-                            title: const Text('Failed to update tour'),
-                            content:
-                                Text('Status code: ${response.statusCode}'),
-                            actions: [
-                              TextButton(
-                                onPressed: () => Navigator.pop(context),
-                                child: const Text('OK'),
-                              ),
-                            ],
-                          ),
-                        );
-                      }
+                      // final http.Response response = await IPHandler()
+                      //     .post('/api/update/tour/${widget.tour.id}', options);
+                      // if (response.statusCode == 200) {
+                      //   Navigator.pop(context);
+                      // } else {
+                      //   showDialog(
+                      //     context: context,
+                      //     builder: (context) => AlertDialog(
+                      //       title: const Text('Failed to update tour'),
+                      //       content:
+                      //           Text('Status code: ${response.statusCode}'),
+                      //       actions: [
+                      //         TextButton(
+                      //           onPressed: () => Navigator.pop(context),
+                      //           child: const Text('OK'),
+                      //         ),
+                      //       ],
+                      //     ),
+                      //   );
+                      // }
                     },
                     style: OutlinedButton.styleFrom(
                       backgroundColor: Colors.green,
@@ -132,48 +130,49 @@ class _TourEditViewState extends State<TourEditView> {
                   const SizedBox(width: 16.0),
                   OutlinedButton(
                     onPressed: () async {
-                      final confirmDelete = await showDialog<bool>(
-                        context: context,
-                        builder: (context) => AlertDialog(
-                          title: const Text('Confirm Deletion'),
-                          content: const Text(
-                            'Are you sure you want to delete this tour?',
-                          ),
-                          actions: [
-                            TextButton(
-                              onPressed: () => Navigator.pop(context, false),
-                              child: const Text('Cancel'),
-                            ),
-                            TextButton(
-                              onPressed: () => Navigator.pop(context, true),
-                              child: const Text('Delete'),
-                            ),
-                          ],
-                        ),
-                      );
-                      if (confirmDelete == true) {
-                        final http.Response response = await IPHandler()
-                            .post('/api/tour/delete/${widget.tour.id}');
+                      // Will update if and when the API endpoint is ready
+                      // final confirmDelete = await showDialog<bool>(
+                      //   context: context,
+                      //   builder: (context) => AlertDialog(
+                      //     title: const Text('Confirm Deletion'),
+                      //     content: const Text(
+                      //       'Are you sure you want to delete this tour?',
+                      //     ),
+                      //     actions: [
+                      //       TextButton(
+                      //         onPressed: () => Navigator.pop(context, false),
+                      //         child: const Text('Cancel'),
+                      //       ),
+                      //       TextButton(
+                      //         onPressed: () => Navigator.pop(context, true),
+                      //         child: const Text('Delete'),
+                      //       ),
+                      //     ],
+                      //   ),
+                      // );
+                      // if (confirmDelete == true) {
+                      //   final http.Response response = await IPHandler()
+                      //       .post('/api/tour/delete/${widget.tour.id}');
 
-                        if (response.statusCode == 200) {
-                          Navigator.pop(context);
-                        } else {
-                          showDialog(
-                            context: context,
-                            builder: (context) => AlertDialog(
-                              title: const Text('Failed to delete tour'),
-                              content:
-                                  Text('Status code: ${response.statusCode}'),
-                              actions: [
-                                TextButton(
-                                  onPressed: () => Navigator.pop(context),
-                                  child: const Text('OK'),
-                                ),
-                              ],
-                            ),
-                          );
-                        }
-                      }
+                      //   if (response.statusCode == 200) {
+                      //     Navigator.pop(context);
+                      //   } else {
+                      //     showDialog(
+                      //       context: context,
+                      //       builder: (context) => AlertDialog(
+                      //         title: const Text('Failed to delete tour'),
+                      //         content:
+                      //             Text('Status code: ${response.statusCode}'),
+                      //         actions: [
+                      //           TextButton(
+                      //             onPressed: () => Navigator.pop(context),
+                      //             child: const Text('OK'),
+                      //           ),
+                      //         ],
+                      //       ),
+                      //     );
+                      //   }
+                      // }
                     },
                     style: OutlinedButton.styleFrom(
                       backgroundColor: Colors.red,
